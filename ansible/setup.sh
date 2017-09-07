52c77fc233cea5def66c80e5a47d3397fcc186a2
Sebuuma Daniel <sedzsoft@gmail.com>
2017-09-07 06:44:31 +0300

    Adds bucketlist playbook, a setup script to install the base software, an inventory that points to the ubuntu instance and a Vagrantfile to spin up the vagrant machine.

---
+++ ansible/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install ansible git
+sudo apt-get -y install nodejs npm
+sudo apt-get -y install python-pip python-dev build-essential libpq-dev
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
+sudo pip install uwsgi
+sudo npm install -g bower
+
+# Connect nginx to uwsgi
+# cd into the /app
+# pip intall -r requirements.txt
+# Migrate the postgres database
+# cd into the /client
+# bower install
+# npm intall
+# ng build
