92d1186e29a67e2a23b16b1017864bd3903de915
Sebuuma Daniel <sedzsoft@gmail.com>
2017-09-08 15:01:19 +0300

    Updates script to deploying frontend

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install ansible git
-sudo apt-get -y install npm
-sudo npm install -g n
+sudo apt-get -y install ansible git curl
+
 sudo apt-get -y install python-pip python-dev build-essential libpq-dev
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 sudo pip install psycopg2
-sudo npm install -g bower
 
 # Connect nginx to uwsgi
 # cd into the /app
